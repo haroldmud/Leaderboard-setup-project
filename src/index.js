ba2dc93fe9495a9b3393a13abc1e7665a6696fff
haroldmud <haroldmudosa@gmail.com>
2022-05-11 13:52:16 +0200

    Fix phrases

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './style.css';
 
 document.body.innerHTML = `<main><h1>Leaderboard</h1>
 <section class="contain"><div class="recentBock"><div class="recentScore">
-<h2 class="recentHead">Recent Score</h2>
+<h2 class="recentHead">Recent Scores</h2>
 <button class="recentBtn">Refresh</button></div>
 <div class="names"><p>name:100</p>
 <p class="grey">name:20</p><p>name:50</p><p class="grey">name:20</p>
